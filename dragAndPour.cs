0d4f16f86792545e00ba47ea34b2c96b30265c06
Rami <nsayed2018@gmail.com>
2017-10-08 14:50:04 -0400

    Add files via upload
    Finished sounds and imported untextured blender assets from Thomas and Mike.

--- dragAndPour.cs
+++ dragAndPour.cs
@@ -11,6 +11,7 @@ public class dragAndPour : MonoBehaviour
     private Vector3 screenPoint;
     private Vector3 offset;
     private bool isCorrect = false;
+    private AudioSource audioSource;
 
     private void Awake()
     {
@@ -19,8 +20,26 @@ public class dragAndPour : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(Resources.Load("SFX_CORRECT") as AudioClip);
+    }
+
     void OnMouseDown()
     {
+        if (isCorrect){
+            gameObject.GetComponent<AudioSource>().PlayOneShot(Resources.Load("SUGAR") as AudioClip);
+            gameObject.GetComponent<AudioSource>().PlayOneShot(Resources.Load("sfx_correct") as AudioClip);
+        }
+        else
+        {
+           gameObject.GetComponent<AudioSource>().PlayOneShot(Resources.Load("sfx_incorrect") as AudioClip);
+        }
+        gameObject.GetComponent<AudioSource>().PlayOneShot(Resources.Load("sfx_"+gameObject.name) as AudioClip);
+
+
+
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
@@ -30,6 +49,7 @@ public class dragAndPour : MonoBehaviour
         Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
         transform.position = cursorPosition;
+      //  transform.position.y = 1;
     }
 
     void OnTriggerEnter(Collider other)
